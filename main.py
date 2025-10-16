adf03fd15c76d43a7d6377fce1d75d8f2b2a2547
cui1fan <1819285039@qq.com>
2025-10-16 17:37:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ letter_count = 0
 digit_count = 0
 space_count = 0
 other_count = 0
-
 input_str = input("请输入一行字符：")
-
 for char in input_str:
     if char.isalpha(): 
         letter_count += 1
@@ -14,7 +12,6 @@ for char in input_str:
         space_count += 1
     else:  
         other_count += 1
-
 print("英文字符:", letter_count)
 print("数字:", digit_count)
 print("空格:", space_count)
