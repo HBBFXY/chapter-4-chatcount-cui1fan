c7690ffd305dc52a2ac381cde5408e7b571d7806
cui1fan <1819285039@qq.com>
2025-10-16 17:39:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ letter_count = 0
 digit_count = 0
 space_count = 0
 other_count = 0
+
 input_str = input("请输入一行字符：")
+
+# 遍历每个字符进行统计
 for char in input_str:
-    if char.isalpha(): 
+    if char.isalpha():  
         letter_count += 1
-    elif char.isdigit(): 
+    elif char.isdigit():  
         digit_count += 1
-    elif char.isspace():  
+    elif char.isspace(): 
         space_count += 1
-    else:  
+    else: 
         other_count += 1
-print("英文字符:", letter_count)
-print("数字:", digit_count)
-print("空格:", space_count)
-print("其他字符:", other_count)
+
+print(f"英文字符: {letter_count}")
+print(f"数字: {digit_count}")
+print(f"空格: {space_count}")
+print(f"其他字符: {other_count}")
