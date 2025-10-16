6e1ac6a393255c4bbdd62c590e087d89096ee214
cui1fan <1819285039@qq.com>
2025-10-16 17:35:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# 这个文件用于编写代码
+letter_count = 0
+digit_count = 0
+space_count = 0
+other_count = 0
+
+input_str = input("请输入一行字符：")
+
+for char in input_str:
+    if char.isalpha(): 
+        letter_count += 1
+    elif char.isdigit(): 
+        digit_count += 1
+    elif char.isspace():  
+        space_count += 1
+    else:  
+        other_count += 1
+
+print("英文字符:", letter_count)
+print("数字:", digit_count)
+print("空格:", space_count)
+print("其他字符:", other_count)
